c8744d5c27fcc1193f3a577b697c3ad982c3e27e
dbunk903 <dbunk903@gmail.com>
2022-05-16 22:40:08 +0900

    Step 5

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -12,6 +12,7 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     var tipAmount:Double = 0.0
+    var numberOfPeople = 2
     @IBAction func tipChanged(_ sender: UIButton) {
         if sender == zeroPctButton{
             zeroPctButton.isSelected = true
@@ -30,11 +31,14 @@ class CalculatorViewController: UIViewController {
             tipAmount = 0.2
         }
     }
-    
+
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        // splitNumberLabel.text = String(Int(sender.value))
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(tipAmount)
+        print(numberOfPeople)
     }
 }
 
