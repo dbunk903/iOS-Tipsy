b8c677a99db028667089e877a79e7341d67a83ac
dbunk903 <dbunk903@gmail.com>
2022-05-16 16:38:30 +0900

    Step 4 - 1 : isSelected to all the buttons

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -12,6 +12,19 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     @IBAction func tipChanged(_ sender: UIButton) {
+        if sender == zeroPctButton{
+            zeroPctButton.isSelected = true
+            tenPctButton.isSelected = false
+            twentyPctButton.isSelected = false
+        } else if sender == tenPctButton{
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = true
+            twentyPctButton.isSelected = false
+        } else if sender == twentyPctButton{
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = false
+            twentyPctButton.isSelected = true
+        }
     }
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
