c1dd7438108fd58ac65820facb5dd3386f6b4f43
dbunk903 <dbunk903@gmail.com>
2022-05-16 16:42:22 +0900

    Step 4 Done : Calculate Pressed

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -11,24 +11,30 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    var tipAmount:Double = 0.0
     @IBAction func tipChanged(_ sender: UIButton) {
         if sender == zeroPctButton{
             zeroPctButton.isSelected = true
             tenPctButton.isSelected = false
             twentyPctButton.isSelected = false
+            tipAmount = 0.0
         } else if sender == tenPctButton{
             zeroPctButton.isSelected = false
             tenPctButton.isSelected = true
             twentyPctButton.isSelected = false
+            tipAmount = 0.1
         } else if sender == twentyPctButton{
             zeroPctButton.isSelected = false
             tenPctButton.isSelected = false
             twentyPctButton.isSelected = true
+            tipAmount = 0.2
         }
     }
+    
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(tipAmount)
     }
 }
 
