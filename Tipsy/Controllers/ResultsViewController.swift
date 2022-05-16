8e7ae53f55183795ad9bd99815efcd1b21adc9bc
dbunk903 <dbunk903@gmail.com>
2022-05-16 16:06:12 +0900

    Step 3 : Create IBActions and IBOutlets for the Results Screen

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultsViewControllerViewController.swift
+//  Tipsy
+//
+//  Created by Jero Lee on 2022/05/16.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
